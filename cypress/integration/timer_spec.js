fea92de321a8480dfd98b21abf23f581d244dee6
Dey <sdey3002@7-11.com>
2020-10-30 22:27:17 +0530

    second commit

--- cypress/integration/timer_spec.js
+++ cypress/integration/timer_spec.js
@@ -1,7 +1,18 @@
+
+
 describe("toggle",function(){
     it("timer",function(){
+      
         cy.visit("./index.html")
-        cy.get("#toggle")
-        cy.get("#print")
+        cy.get("#play").click()
+        cy.get("#print").then((val)=>{
+            console.log(val.text());
+        })
+        cy.get("#pause").click()
+         
+        
+        
+      
+     
     })
 })
--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 
 
-    var info = document.getElementById("print");
-    var toggle = document.getElementById("toggle");
-    var i = 0;
-   
-    var btn = 1;
-    
-    interval = null;
-    
-      function add () {
-      
-            if (btn) {
-    
-                btn = 0;
-   
-                interval = setInterval(function() { 
-                    info.innerHTML = i++;
-                }, 1);
-            }
-  
-            else {
-  
-                btn = 1;
-                i=0;
-    
-                clearInterval(interval);
-                info.innerHTML=i;
-            }
-        };
-    
+var info = document.getElementById("print");
+var toggle = document.getElementById("toggle");
+var i = 0;
+
+var btn = 1;
+
+interval = null;
+
+function add() {
+
+    if (btn) {
+
+        btn = 0;
+
+        interval = setInterval(function () {
+            info.innerHTML = i++;
+        }, 1000);
+    }
+
+
+};
+function destroy() {
+
+    btn = 1
+    info.innerHTML = null
+    clearInterval(interval)
+
+
+};
